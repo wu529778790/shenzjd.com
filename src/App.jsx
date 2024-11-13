2441050a31b5714e64ec90887017a5222f911511
wuwei <wuwei@fadada.com>
2024-11-13 16:16:48 +0800

    Refactor navigation component and styles, add alias for src in vite config

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,12 @@
+import Navigation from "@/components/Navigation";
 import "./App.css";
 
-export default App;
-import { useState } from "react";
-import data from "./data/navigation.json";
-
 function App() {
-  const [navData] = useState(data);
   return (
-    <div className="nav-container">
-      {navData?.map((category) => (
-        <div key={category.category} className="category">
-          <h2>{category.category}</h2>
-          <div className="links-grid">
-            {category.sites.map((site) => (
-              <a
-                key={site.name}
-                href={site.url}
-                target="_blank"
-                className="nav-link">
-                {site.name}
-              </a>
-            ))}
-          </div>
-        </div>
-      ))}
+    <div>
+      <Navigation />
     </div>
   );
 }
+
+export default App;
---
+++ src/components/Navigation/index.jsx
@@ -0,0 +1,27 @@
+import "./index.css";
+import data from "@/data/navigation.json";
+
+function Navigation() {
+  return (
+    <div className="nav-container">
+      {data?.map((category) => (
+        <div key={category.category} className="category">
+          <h2>{category.category}</h2>
+          <div className="links-grid">
+            {category.sites.map((site) => (
+              <a
+                key={site.name}
+                href={site.url}
+                target="_blank"
+                className="nav-link">
+                {site.name}
+              </a>
+            ))}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Navigation;
