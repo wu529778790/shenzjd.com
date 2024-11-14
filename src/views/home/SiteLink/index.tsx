71e2d04a40b897ac46081de980473a7760a048f2
wuwei <wuwei@fadada.com>
2024-11-14 20:53:20 +0800

    Update navigation.json and SiteLink component for improved UI and content

--- src/views/home/SiteLink/index.tsx
+++ src/views/home/SiteLink/index.tsx
@@ -9,21 +9,24 @@ import { Site } from "../types";
 
 function SiteLink({ site }: { site: Site }) {
   return (
-    <Card className="block p-4 bg-white border border-gray-200 rounded-lg text-center transition-all duration-300 ease-in-out hover:transform hover:-translate-y-1 hover:shadow-lg hover:border-gray-300 hover:text-blue-600">
-      <a href={site.url} target="_blank" className="text-decoration-none">
-        <div className="flex justify-center items-center h-16 w-16 bg-gray-100 rounded-full overflow-hidden">
+    <Card className="flex p-2 bg-white border border-gray-200 rounded-lg transition-all duration-300 ease-in-out hover:transform hover:-translate-y-0.5 hover:shadow-md hover:border-gray-300 hover:text-blue-600">
+      <a
+        href={site.url}
+        target="_blank"
+        className="flex items-center text-decoration-none">
+        <div className="flex-shrink-0 h-8 w-8 bg-gray-100 rounded-full overflow-hidden">
           <img
             src={site.image}
             alt={site.name}
             className="max-w-full h-auto rounded-lg"
           />
         </div>
-        <CardContent className="p-2">
-          <CardHeader className="flex flex-col space-y-1.5 p-6">
-            <CardTitle className="text-xl font-bold mb-1">
+        <CardContent className="flex flex-col justify-center ml-2">
+          <CardHeader className="flex flex-col space-y-0.75">
+            <CardTitle className="text-lg font-bold mb-0.5">
               {site.name}
             </CardTitle>
-            <CardDescription className="text-base text-gray-600">
+            <CardDescription className="text-sm text-gray-600 truncate">
               {site.description}
             </CardDescription>
           </CardHeader>
