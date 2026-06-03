1180c5f7a472f5bf66a86fc314bec104f9e0ca2e
wuwei <wuwei@fadada.com>
2026-06-04 00:05:27 +0800

    Fix sitemap URLs: derive origin from Host header instead of Docker internal address

--- src/app/sitemap.xml/route.ts
+++ src/app/sitemap.xml/route.ts
@@ -1,9 +1,23 @@
 import { getChannelInfo } from '../../lib/sources'
 import { getEnv } from '../../lib/env'
 
+function getOrigin(request: Request, url: URL): string {
+  const siteUrl = getEnv('SITE_URL')
+  if (siteUrl) return siteUrl
+
+  // Derive from Host header (works behind reverse proxy / Docker)
+  const host = request.headers.get('host')
+  if (host) {
+    const proto = request.headers.get('x-forwarded-proto') ?? 'https'
+    return `${proto}://${host}`
+  }
+
+  return url.origin
+}
+
 export async function GET(request: Request) {
   const url = new URL(request.url)
-  const siteUrl = getEnv('SITE_URL') ?? url.origin
+  const origin = getOrigin(request, url)
   const TAGS = getEnv('TAGS')
   const LINKS = getEnv('LINKS')
   const channel = await getChannelInfo()
@@ -16,7 +30,7 @@ export async function GET(request: Request) {
 
   const staticSitemaps = staticPages.map(page => `
 <sitemap>
-  <loc>${siteUrl}${page ? page + '/' : ''}</loc>
+  <loc>${origin}${page ? page + '/' : ''}</loc>
 </sitemap>`).join('')
 
   // Post sub-sitemaps
@@ -32,7 +46,7 @@ export async function GET(request: Request) {
 
   const postSitemaps = pages.map(page => `
 <sitemap>
-  <loc>${url.origin}/sitemap/${page}.xml</loc>
+  <loc>${origin}/sitemap/${page}.xml</loc>
 </sitemap>`).join('')
 
   return new Response(`<?xml version="1.0" encoding="UTF-8"?>
--- src/app/sitemap/[cursor]/route.ts
+++ src/app/sitemap/[cursor]/route.ts
@@ -1,17 +1,27 @@
 import { getChannelInfo } from '../../../lib/sources'
 
+function getOrigin(request: Request, url: URL): string {
+  const host = request.headers.get('host')
+  if (host) {
+    const proto = request.headers.get('x-forwarded-proto') ?? 'https'
+    return `${proto}://${host}`
+  }
+  return url.origin
+}
+
 export async function GET(
   request: Request,
   { params }: { params: Promise<{ cursor: string }> },
 ) {
   const { cursor } = await params
   const url = new URL(request.url)
+  const origin = getOrigin(request, url)
   const channel = await getChannelInfo({ before: cursor })
   const posts = channel.posts || []
 
   const xmlUrls = posts.map(post => `
     <url>
-      <loc>${url.origin}/posts/${post.id}</loc>
+      <loc>${origin}/posts/${post.id}</loc>
       <lastmod>${new Date(post.datetime).toISOString()}</lastmod>
     </url>`).join('')
 
